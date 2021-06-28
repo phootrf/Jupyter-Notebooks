bb133dd05d5d6d4de0ab92d671b2ae856284bc8c
RFL <63897048+phootrf@users.noreply.github.com>
2021-06-28 14:57:19 +0200

    Update start-jupyter.sh

--- start-jupyter.sh
+++ start-jupyter.sh
@@ -3,4 +3,4 @@
 # Run the command on container startup
 sudo cron
 
-jupyter notebook --ip 0.0.0.0 --notebook-dir=$HOME/Jupyter-Notebooks/notebooks --NotebookApp.token=''
+jupyter notebook --ip 0.0.0.0 --notebook-dir=$HOME/Jupyter-Notebooks/Programmieren --NotebookApp.token=''
